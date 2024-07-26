462b97c225be0426f397dcea3c155d3a41c10c9a
Jefferson O. <jefferson240111@gmail.com>
2024-07-26 09:06:22 -0500

    Update vista.py

--- vista.py
+++ vista.py
@@ -1,1 +1,10 @@
+#vista
+import tkinter as tk
 
+class Vista(tk.Tk):
+    def __init__(self,controlador):
+        super().__init__()
+        self.controlador = controlador
+        self.title("principal")
+        
+        self 
