2cc2947b0d9963408eb26a3c0e95a55591cf861d
Jefferson O. <jefferson240111@gmail.com>
2024-07-29 22:17:58 -0500

    Update vista.py No2

--- vista.py
+++ vista.py
@@ -3,40 +3,8 @@ import tkinter as tk
 from tkinter import messagebox
 import json
 
-class Public:
-    def __init__(self):
-        self.datopersona = {
-            "nombre": None,
-            "apellido": None,
-            "edad": None,
-            "sexo": None
-        }
-
-    def almacenar_datos(self):
-        Private().almacenar(self.datopersona)
-
-    def recibir_dato(self, index):
-        return Private().devolver_dato_get(index)
-
-
-class Private:
-    def __init__(self):
-        self.almacenador = []
-
-    def almacenar(self, dato):
-        self.almacenador.append(dato)
-
-    def devolver_dato_get(self, index):
-        if index < len(self.almacenador):
-            return self.almacenador[index]
-        else:
-            return None
-
-
-class App:
+class Vista:
     def __init__(self, root):
-        self.public = Public()
-
         self.root = root
         self.root.title("Gestión de Datos Personales")
 
@@ -63,30 +31,31 @@ class App:
         self.sexo_entry = tk.Entry(self.frame)
         self.sexo_entry.grid(row=3, column=1)
 
-        self.save_button = tk.Button(self.frame, text="Guardar", command=self.save_data)
+        self.save_button = tk.Button(self.frame, text="Guardar", command=self.guardarDatos)
         self.save_button.grid(row=4, column=0, columnspan=2)
 
-        self.display_button = tk.Button(self.frame, text="Mostrar Datos", command=self.display_data)
+        self.display_button = tk.Button(self.frame, text="Mostrar Datos", command=self.mostrarDatos)
         self.display_button.grid(row=5, column=0, columnspan=2)
 
-    def save_data(self):
-        self.public.datopersona["nombre"] = self.nombre_entry.get()
-        self.public.datopersona["apellido"] = self.apellido_entry.get()
-        self.public.datopersona["edad"] = self.edad_entry.get()
-        self.public.datopersona["sexo"] = self.sexo_entry.get()
+        self.datos_almacenados = []
 
-        self.public.almacenar_datos()
+    def guardarDatos(self):
+        datos_persona = {
+            "nombre": self.nombre_entry.get(),
+            "apellido": self.apellido_entry.get(),
+            "edad": self.edad_entry.get(),
+            "sexo": self.sexo_entry.get()
+        }
+        
+        self.datos_almacenados.append(datos_persona)
         messagebox.showinfo("Información", "Datos guardados correctamente")
 
-    def display_data(self):
-        datos = []
-        for i in range(len(Private().almacenador)):
-            datos.append(Private().devolver_dato_get(i))
-        messagebox.showinfo("Datos Almacenados", json.dumps(datos, indent=4))
-
+    def mostrarDatos(self):
+        datos_json = json.dumps(self.datos_almacenados, indent=4)
+        messagebox.showinfo("Datos Almacenados", datos_json)
 
 root = tk.Tk()
-app = App(root)
+app = Vista(root)
 root.mainloop()
 
 
