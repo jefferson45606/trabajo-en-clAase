a49cde494f03098b7d78c8f1cf5c0e235c436691
Jefferson O. <jefferson240111@gmail.com>
2024-07-30 07:08:59 -0500

    Update controlador.py v1

--- controlador.py
+++ controlador.py
@@ -1,1 +1,18 @@
+class Controlador:
+    def __init__(self):
+        self.public = Public()
+        self.private = Private()
+
+    def almacenar_datos(self, nombre, apellido, edad, sexo):
+        self.public.datopersona["nombre"] = nombre
+        self.public.datopersona["apellido"] = apellido
+        self.public.datopersona["edad"] = edad
+        self.public.datopersona["sexo"] = sexo
+        self.public.almacenar_datos()
+
+    def obtener_datos(self):
+        datos = []
+        for i in range(len(self.private.almacenador)):
+            datos.append(self.private.devolver_dato_get(i))
+        return datos
 
